d056dbb7e53af0237c809f61dad8d6bf0dd2630a
Price Tula <pricetula@gmail.com>
2024-09-27 00:43:02 +0300

    add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
+<script>
+	import '../app.css';
+</script>
+
 <main>
 	<nav>
 		<a href="/">Home</a>
-        <a href="/users">Users</a>
-        <a href="/todos">Todos</a>
-        <a href="/posts">Posts</a>
-        <a href="/photos">Photos</a>
-        <a href="/comments">Comments</a>
-        <a href="/albums">Albums</a>
+		<a href="/users">Users</a>
+		<a href="/todos">Todos</a>
+		<a href="/posts">Posts</a>
+		<a href="/photos">Photos</a>
+		<a href="/comments">Comments</a>
+		<a href="/albums">Albums</a>
 	</nav>
 
 	<slot />
